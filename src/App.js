bef1156325640017ce94e92fa3f557c39763d9e5
lassad96 <Lewan.nawel95@gmail.com>
2021-09-08 19:37:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Addtask from './component/Addtask'
+import Listtask from './component/Listtask'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  state = {
+    taskList : [{id:0, title: 'html and CSS', isDone: false},
+    {id:1, title: 'JavaScript', isDone: true},
+    {id:2, title: 'React', isDone: false}]
+  }
+  handleDelete = (idItem) => {
+    this.setState({taskList: this.state.taskList.filter(
+    (el) =>  el.id !== idItem
+
+    )})
+  }
+  handleDone =(idItem) => {
+    this.setState({taskList:this.state.taskList.map(
+      (el) => el.id === idItem ? {...el,isDone:!el.isDone}: el
+    )
+     })
+  }
+  handleAdd =(newTitle) => {
+    this.setState({taskList: [...this.state.taskList,
+      {id:Date.now(), title:newTitle, isDone:false}]
+    })
+  }
+
+  // handlefiltre =() => {
+  //   this.setState({taskList:this.state.taskList.map(
+  //     (el) =>{ if (el.isDone===true) 
+  //    { return el} 
+  //   }
+  //   )
+  //    })
+  // }
+  
+  handleEdit=(iditem,newTitle) => {
+    this.setState({taskList: this.state.taskList.map(
+     (el)=> el.id===iditem ?{...el, title:newTitle}:el 
+    )})
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do List</h1>
+        <Addtask handleAdd={this.handleAdd}handlefiltre={this.handlefiltre}/>
+       <Listtask taskList={this.state.taskList} handleEdit={this.handleEdit}handleDelete= {this.handleDelete} handleDone= {this.handleDone}/> 
+       
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
